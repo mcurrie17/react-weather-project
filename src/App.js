cf284fa04eac0553cbfa035e46118a043202e35e
Megan <107447664+mcurrie17@users.noreply.github.com>
2022-09-24 14:21:41 -0500

    Installed bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by <strong>Megan Currie</strong> and is{" "}
+          <a
+            href="https://github.com/mcurrie17"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
